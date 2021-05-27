301a0ce1d4c53190e4d2db9d340193643de735aa
Ian King <kickthechair@gmail.com>
2021-05-27 13:39:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+array = [1,2,3]
 def square_array(array)
-  # your code here
+  squared = array.each { |element| element ** 2 }
+  squared
 end
+
+p square_array(array)
+
+
+#higher level iterator
+#def square_array(array)
+#  array.map { |element| element ** 2 }
+#end
